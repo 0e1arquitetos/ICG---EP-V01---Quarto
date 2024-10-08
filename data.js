361d84bc64b4e1b085cca009133a8bbe1497d649
0e1arquitetos <141720410+0e1arquitetos@users.noreply.github.com>
2024-10-08 15:57:45 -0300

    ICG - EP V01 - Quarto
    Ibis - Quarto

---
+++ data.js
@@ -0,0 +1,73 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0-dormitrio",
+      "name": "Dormitório",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1500,
+      "initialViewParameters": {
+        "yaw": 0.055044999485977186,
+        "pitch": -0.005621013734092628,
+        "fov": 1.3474042771833745
+      },
+      "linkHotspots": [],
+      "infoHotspots": []
+    },
+    {
+      "id": "1-banheiro",
+      "name": "Banheiro",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1500,
+      "initialViewParameters": {
+        "yaw": 0.04372615116170486,
+        "pitch": -0.022543103931782937,
+        "fov": 1.3474042771833745
+      },
+      "linkHotspots": [],
+      "infoHotspots": []
+    }
+  ],
+  "name": "ICG - EP V01 - Quarto",
+  "settings": {
+    "mouseViewMode": "drag",
+    "autorotateEnabled": true,
+    "fullscreenButton": false,
+    "viewControlButtons": false
+  }
+};
